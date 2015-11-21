e98ce96c22b3f0e53f08aab9bd68439ef9997858
Amit Srivastava <srivastava.amit@gmail.com>
2015-11-21 18:16:57 +0530

    more changes

--- Question.py
+++ Question.py
@@ -11,8 +11,40 @@ def conv_choice_list(ch):
         choices[CHOICE_LABELS[i]] = ch[i]
     return choices
 
+class ImageInfo:
+    def __init__(self, center, size, radius = 0, lifespan = None, animated = False):
+        self.center = center
+        self.size = size
+        self.radius = radius
+        if lifespan:
+            self.lifespan = lifespan
+        else:
+            self.lifespan = float('inf')
+        self.animated = animated
 
-class Choice:
+    def get_center(self):
+        return self.center
+
+    def get_size(self):
+        return self.size
+
+    def get_radius(self):
+        return self.radius
+
+    def get_lifespan(self):
+        return self.lifespan
+
+    def get_animated(self):
+        return self.animated
+
+q1_info = ImageInfo([64, 64], [128, 128], 17, 24, True)
+q1_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/explosion_alpha.png")
+
+welcome_sound = simplegui.load_sound("https://www.dropbox.com/s/aboqginatbi5tpe/Welcome.mp3?dl=0")
+wrong_sound   = simplegui.load_sound("https://www.dropbox.com/s/4wy0o9lsdgeh729/Wrong.mp3?dl=0")
+correct_sound = simplegui.load_sound("https://www.dropbox.com/s/stu10xsbzd9svcd/Right.mp3?dl=0")
+
+class Data:
     def __init__(self, text, img = None):
         self._text = text
         self._img = img
@@ -34,12 +66,12 @@ class Question:
         self._correct = CHOICE_LABELS[correct_idx]
     def getInstruction(self):
         return self._question
-    def getCorrectChoice(self):
+    def getCorrectData(self):
         return self._correct
     def __str__(self):
         qstr = "Question: " + self.getInstruction()
         qstr = qstr + '\n'
-        qstr = qstr + "Figure: None\n"
+        # qstr = qstr + "Figure: None\n"
         for ch in CHOICE_LABELS:
             qstr = qstr + ch + ': ' + str(self._choices[ch]) + "\n"
         return qstr
@@ -47,7 +79,7 @@ class Question:
         print self
         if choice != None:
             print 'You entered', choice, ',',
-            if choice == self.getCorrectChoice():
+            if choice == self.getCorrectData():
                 print "Correct!! Good Job!"
             else:
                 print "WRONG! Please try again."
@@ -67,14 +99,14 @@ def main():
     main function for testing
     :return: None
     '''
-    q1 = Question("Test Question", [Choice('1'), Choice('2'), Choice('3'), Choice('4')], 2)
+    q1 = Question("Test Question", [Data('1'), Data('2'), Data('3'), Data('4')], 2)
     q1.printQuestion()
     q1.printQuestion('A')
     q1.printQuestion('C')
     q1.resetQuestion()
     q1.printQuestion()
     q1.printQuestion('C')
-    q1.printQuestion(q1.getCorrectChoice())
+    q1.printQuestion(q1.getCorrectData())
 
 if __name__ == '__main__':
     main()
